9ddcac63a62c8889db15c8b9b4e6e283e1719801
pc <pc@DESKTOP-RK5M37E>
2021-11-02 19:27:41 +0300

    Proje dosyası ekle.

---
+++ Business/Abstract/IProductService.cs
@@ -0,0 +1,14 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Abstract
+{
+   public  interface IProductService
+    {
+        List<Product> GetAll();
+        List<Product> GetAllByCategoryId(int ıd); // GetALL (TÜMÜNÜ GETİR) NEYE GÖRE CategoryID ' SİNE GÖRE
+        List<Product> GetByUnitPrice(decimal min, decimal max);
+    }
+}
---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,34 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal _productDal;
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+        public List<Product> GetAll()
+        {// iş kodları yetkisi varmı?
+            return _productDal.GetAll();
+        }
+
+        public List<Product> GetAllByCategoryId(int ıd)
+        {
+            return _productDal.GetAll(p => p.CategoryId == ıd);// gönderdiğim ıd categoryıd eşitse onları gönder
+        }
+
+        public List<Product> GetByUnitPrice(decimal min, decimal max)
+        {
+            return _productDal.GetAll(p => p.UnitPrice >= min && p.UnitPrice <= max);
+        }
+
+    }
+}
